e27f1b05ca96853640d141c7b2806f4a85f53aa5
bhanusharanya <sharanya2630@gmail.com>
2024-06-30 19:14:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function insert_Row() {
-    //Write your code here
-  
-  
+  //Write your code here
+  let table = document.getElementById("sampleTable");
+
+  let insert_Row = table.insertRow(0);
+
+  let newCell1 = insert_Row.insertCell(0);
+  let newCell2 = insert_Row.insertCell(1);
+
+  newCell1.innerHTML = "New Cell1";
+  newCell2.innerHTML = "New Cell2";
 }
